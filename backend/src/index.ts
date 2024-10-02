226b241ccd876f584d77fd947df55d7a6c589d1b
Lufei1010 <lufeiwu1010@gmail.com>
2024-10-02 14:54:31 +0300

    database_mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,18 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+console.log(
+  "MongoDB Connection String:",
+  process.env.MONGODB_CONNECTION_STRING
+);
+
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database!"));
+  
 
 const app = express();
 app.use(express.json());
