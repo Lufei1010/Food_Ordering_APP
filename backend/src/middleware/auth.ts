90edf287489256bbd2f5faf1129eb71af31eb0cf
Lufei1010 <lufeiwu1010@gmail.com>
2024-10-11 12:12:02 +0300

    UserProfile Update_Validation

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,4 +1,16 @@
-import { auth } from "express-oauth2-jwt-bearer"
+import { Request, Response, NextFunction } from "express";
+import { auth } from "express-oauth2-jwt-bearer";
+import jwt from "jsonwebtoken";
+import User from "../models/user";
+
+declare global {
+  namespace Express {
+    interface Request {
+      auth0Id?: string;
+      userId?: string;
+    }
+  }
+}
 
 export const jwtCheck = auth({
   audience: process.env.AUTH0_AUDIENCE,
@@ -8,3 +20,35 @@ export const jwtCheck = auth({
 //check the authorization header for the bearer token(frontend has bearer token),
 // then it connects to the server to verify the token we get in the request belongs to a logged in user
 //add these to the MyUserRoute.
+
+export const jwtParse = async (
+  req: Request,
+  res: Response,
+  next: NextFunction) => {
+    const { authorization } = req.headers;
+
+    //Bearer lsjadoejedshjjljgionnfdsgh
+    if (!authorization || !authorization.startsWith("Bearer ")) {
+      return res.sendStatus(401);
+    }
+
+    const token = authorization.split(" ")[1]; //Bearer lsjadoejedshjjljgionnfdsgh --become two item seprately
+   
+    try {
+      const decoded = jwt.decode(token) as jwt.JwtPayload;
+      const auth0Id = decoded.sub;
+
+      const user = await User.findOne({ auth0Id });
+
+      if(!user) {
+        return res.sendStatus(401);
+      }
+
+      req.auth0Id = auth0Id as string;  
+      req.userId = user._id.toString();
+      next(); // it is gonna call the updateCurrentUser in Controller
+    } catch (error) {
+      return res.sendStatus(401)
+    }
+  };
+   // these code above use in controller but these logic repeated for every single request Handler so build it separately.
--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
-import { jwtCheck } from '../middleware/auth';
+import { jwtCheck, jwtParse } from '../middleware/auth';
+import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
 // sets up a POST endpoint at /api/my/users
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
+router.put("/", jwtCheck, jwtParse, validateMyUserRequest, MyUserController.updateCurrentUser);
+
 
 export default router;
+//handler and controller that handles the request to create a new user
