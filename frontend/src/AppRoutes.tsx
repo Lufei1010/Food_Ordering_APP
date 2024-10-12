b2b7f037c7d98036efb1586460a038ab649b711d
Lufei1010 <lufeiwu1010@gmail.com>
2024-10-12 22:33:43 +0300

    Protect user_ profile route

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -3,6 +3,7 @@ import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./auth/ProtectedRoute";
 
 const AppRoutes = () => {
   return (
@@ -11,19 +12,25 @@ const AppRoutes = () => {
       <Route
         path="/"
         element={
-          <Layout showHero> 
-          {/* it is true, then UserProfilePage is false */}
+          <Layout showHero>
+            {/* it is true, then UserProfilePage is false */}
             <HomePage />
           </Layout>
         }
       />
       <Route path="auth-callback" element={<AuthCallbackPage />} />
-      <Route path="/user-profile" 
-      element={
-      <Layout>
-        <UserProfilePage />
-        </Layout>}
-      />
+
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/user-profile"
+          element={
+            <Layout>
+              <UserProfilePage />
+            </Layout>
+          }
+        />
+      </Route>
+
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
