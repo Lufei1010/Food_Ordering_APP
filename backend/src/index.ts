2455147038911285a96bc9a4975efa03a9afad3f
Lufei1010 <lufeiwu1010@gmail.com>
2024-10-13 14:38:45 +0300

    backend_/health &  cloudinary setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
 
 
 console.log(
@@ -14,6 +15,13 @@ console.log(
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("Connected to database!"));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
   
 
 const app = express();
@@ -23,6 +31,10 @@ app.use(cors());
 // app.get("/test", async (req: Request, res: Response) => {
 //     res.json({ message: "Hello!"})
 // }); test point needed in the beginning
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.json({ message: "health OK!"})
+})// basic endpoint to check if the server has successfully started
 
 app.use("/api/my/user", myUserRoute);
 // it tells Express to send any requests starting with /api/my/user 
