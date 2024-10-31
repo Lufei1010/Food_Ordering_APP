fe6cc575e26417ca4e87ee306bcb0e29af5f9f89
Lufei1010 <lufeiwu1010@gmail.com>
2024-10-31 23:53:08 +0200

    Pre-Populate Restaurant Form - shows created restaurant record

--- frontend/src/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm.tsx
+++ frontend/src/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -9,6 +9,8 @@ import MenuSection from "./MenuSection";
 import ImageSection from "./ImageSection";
 import LoadingButton from "@/components/LoadingButton";
 import { Button } from "@/components/ui/button";
+import { Restaurant } from "@/types";
+import { useEffect } from "react";
 
 const formSchema = z.object({
   restaurantName: z.string({
@@ -49,12 +51,13 @@ const formSchema = z.object({
 type RestaurantFormData = z.infer<typeof formSchema>;
 
 type Props = {
+  restaurant?: Restaurant; //? undefined
   onSave: (restaurantFormData: FormData) => void;
   isLoading: boolean;
 }
 
 //component
-const ManageRestaurantForm = ({onSave, isLoading}: Props) => {
+const ManageRestaurantForm = ({onSave, isLoading, restaurant }: Props) => {
   const form = useForm<RestaurantFormData>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -62,6 +65,28 @@ const ManageRestaurantForm = ({onSave, isLoading}: Props) => {
       menuItems: [{ name: "", price: 0 }],
     },
   });
+
+  useEffect(() => {
+    if (!restaurant) {
+      return;
+    }
+//price lowest domination of 100 = 100pence == 1GPB
+  const deliveryPriceFormatted = parseInt(
+    (restaurant.deliveryPrice / 100).toFixed(2)
+  );
+    
+  const menuItemFormatted = restaurant.menuItems.map((item) => ({
+    ...item,
+    price: parseInt((item.price / 100).toFixed(2))
+  }));
+  
+  const updateRestaurant = {
+    ...restaurant,
+    deliveryPrice: deliveryPriceFormatted,
+    menuItems: menuItemFormatted,
+  }
+  form.reset(updateRestaurant);
+  }, [form, restaurant])
 
 const onSubmit = (formDataJson: RestaurantFormData) => {
   const formData = new FormData();
